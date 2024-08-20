5b5c1b4d8a94a4428c026f68a2e9e870d59d2f32
Andrew Settle <andsettle1@gmail.com>
2024-08-20 10:19:27 -0400

    uploading pickaxe

---
+++ intro/puts_examples.rb
@@ -0,0 +1,4 @@
+"gin joint".length  # => 9
+"Rick".index("c")   # => 2
+42.even?            # => true 
+sam.play(song)
---
+++ tut_classes/stock_stats/book_in_stock_1.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# This is the first version of the BookInStock class
+class BookInStock
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+end
+
+b1 = BookInStock.new("isbn1", 3)
+p b1
+
+b2 = BookInStock.new("isbn2", 3.14)
+p b2
+
+b3 = BookInStock.new("isbn3", "5.67")
+p b3
+
